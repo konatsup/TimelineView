74430b3b590e164a6292a98d6d912f79a16b7007
konatsup <konatsukomatsu@gmail.com>
2018-10-28 00:03:27 +0900

    Fix trackList to NonNull property

--- timelineview/src/main/java/com/konatsup/timelineview/TimelineView.kt
+++ timelineview/src/main/java/com/konatsup/timelineview/TimelineView.kt
@@ -2,27 +2,25 @@ package com.konatsup.timelineview
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.support.annotation.NonNull
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
 import android.widget.FrameLayout
-import android.widget.Toast
 
 import java.util.ArrayList
 import java.util.HashMap
 
 class TimelineView : FrameLayout, AdapterView.OnItemClickListener {
 
-    var timelineType: TimelineType? = null
-    var trackList: ArrayList<Track>? = null
+    var timelineType: TimelineType = TimelineType.TIME
+    var trackList: ArrayList<Track> = ArrayList()
     var currentPosition: Int = 0
     var columnCount: Int = 0
         set(columnCount) {
             field = columnCount
-            fglm.setTotalColumnCount(this.columnCount)
+            fglm.totalColumnCount = this.columnCount
         }
 
 
@@ -31,12 +29,11 @@ class TimelineView : FrameLayout, AdapterView.OnItemClickListener {
     private val fglm = FixedGridLayoutManager()
 
     val trackCount: Int
-        get() = requireNotNull(trackList).size
+        get() = trackList.size
 
     constructor(context: Context) : super(context) {}
 
-    @JvmOverloads constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int = 0) : super(context, attrs, defStyleAttr) {
-
+    @JvmOverloads constructor(cont: Context, attrs: AttributeSet, defStyleAttr: Int = 0) : super(cont, attrs, defStyleAttr) {
 
         val inflater = LayoutInflater.from(context)
         val rootView = inflater.inflate(R.layout.timeline_view, this, true)
@@ -45,7 +42,7 @@ class TimelineView : FrameLayout, AdapterView.OnItemClickListener {
 
         adapter.setOnItemClickListener(this)
 
-        fglm.setTotalColumnCount(0)
+        fglm.totalColumnCount = 0
 
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = fglm
@@ -56,7 +53,7 @@ class TimelineView : FrameLayout, AdapterView.OnItemClickListener {
 
     /* functions */
     fun addTrack(track: Track) {
-        trackList!!.add(track)
+        trackList.add(track)
         adapter.update(trackList, this.columnCount)
         adapter.notifyDataSetChanged()
     }
@@ -65,28 +62,28 @@ class TimelineView : FrameLayout, AdapterView.OnItemClickListener {
         @SuppressLint("UseSparseArrays")
         val map = HashMap<Int, Composition>()
         val track = Track(map)
-        trackList!!.add(track)
+        trackList.add(track)
         adapter.update(trackList, this.columnCount)
         adapter.notifyDataSetChanged()
     }
 
     fun updateTrack(index: Int, track: Track) {
-        trackList!![index] = track
+        trackList[index] = track
         adapter.update(trackList, this.columnCount)
         adapter.notifyDataSetChanged()
     }
 
     fun deleteTrackForPosition(pos: Int) {
-        trackList!!.removeAt(pos)
+        trackList.removeAt(pos)
         adapter.update(trackList, this.columnCount)
         adapter.notifyDataSetChanged()
     }
 
     fun getTrack(position: Int): Track {
-        return trackList!![position]
+        return trackList[position]
     }
 
-    override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+    override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
         // do Click Action
 
     }
